58d2adc55e6ef1538b280133365b9891bde158d0
fable54321 <timothe5@hotmail.com>
2026-05-04 08:54:09 -0400

    first commit

--- src/App/5000--VisitorsSection/100--VisitorsMainPage/1001--VisitorsHome/VisitorsHome.tsx
+++ src/App/5000--VisitorsSection/100--VisitorsMainPage/1001--VisitorsHome/VisitorsHome.tsx
@@ -3,7 +3,7 @@ import vegibecLogo from '../../../../assets/vegibec.png'
 
 const VisitorsHome = () => {
   return (
-    <section className="flex flex-col items-center gap-10 w-[min(98%,500px)] ">
+    <section className="flex flex-col items-center gap-10 w-[min(98%,600px)] ">
         <img src={vegibecLogo} alt="Vegibec Logo" className=' w-80 mt-10 tablet:w-150' />
        <div className="flex flex-col  gap-4 w-full">
       <Link to="arrivee"  className="bg-secondary font-primary font-bold rounded-sm py-1 text-center text-white text-[2.9em] " >J'arrive</Link>
--- src/App/5000--VisitorsSection/100--VisitorsMainPage/VisitorsMainPage.tsx
+++ src/App/5000--VisitorsSection/100--VisitorsMainPage/VisitorsMainPage.tsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom"
 
 const VisitorsMainPage = ( { token } : { token: string } ) => {
   return (
-    <article className="pt-2 flex flex-col items-center font-secondary pb-4 max-sm[0.85rem] max-md:mt-40">
+    <article className="pt-2 flex flex-col items-center font-secondary pb-4 max-sm[0.85rem] lg:mt-0 tablet:mt-60">
       <h2 className="text-[3.5em] font-bold font-primary text-secondary text-center">Registre des visiteurs</h2>
       <Outlet context={{ token }} />
     </article>
