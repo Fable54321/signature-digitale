715762c5d5336000329af1c0f8b960bc8def1ddc
Timothe Bissonnette <timothe5@hotmail.com>
2026-05-04 16:45:41 -0400

    first commit

--- src/App/5000--VisitorsSection/100--VisitorsMainPage/1001--VisitorsHome/VisitorsHome.tsx
+++ src/App/5000--VisitorsSection/100--VisitorsMainPage/1001--VisitorsHome/VisitorsHome.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom"
+import { ArrowRight, LogIn, LogOut } from "lucide-react"
 import vegibecLogo from '../../../../assets/vegibec.png'
 
 const VisitorsHome = () => {
@@ -6,8 +7,24 @@ const VisitorsHome = () => {
     <section className="flex flex-col items-center gap-10 w-[min(98%,800px)] ">
         <img src={vegibecLogo} alt="Vegibec Logo" className=' w-80 mt-10 tablet:w-150' />
        <div className="flex flex-col  gap-4 w-full">
-      <Link to="arrivee"  className="bg-secondary font-primary font-bold rounded-sm py-1 text-center text-white text-[2.9em] tablet:text-[3.5em]" >J'arrive</Link>
-      <Link to="depart" className="bg-secondary font-primary font-bold rounded-sm py-1 text-center text-white text-[2.9em] tablet:text-[3.5em]">Je quitte</Link>
+      <Link to="arrivee"  className="bg-secondary font-primary font-bold rounded-sm py-1 px-5 text-white text-[2.9em] tablet:text-[4.5em] flex items-center justify-between gap-4" >
+        <span className="flex items-center gap-4">
+          <span className="flex h-[1.2em] w-[1.2em] items-center justify-center rounded-full bg-tertiary p-3 text-secondary">
+            <LogIn className="h-full w-full" strokeWidth={2.5} />
+          </span>
+          J'arrive
+        </span>
+        <ArrowRight className="h-[0.8em] w-[0.8em]" strokeWidth={2.5} />
+      </Link>
+      <Link to="depart" className="bg-secondary font-primary font-bold rounded-sm py-1 px-5 text-white text-[2.9em] tablet:text-[4.5em] flex items-center justify-between gap-4">
+        <span className="flex items-center gap-4">
+          <span className="flex h-[1.2em] w-[1.2em] items-center justify-center rounded-full bg-tertiary p-3 text-secondary">
+            <LogOut className="h-full w-full" strokeWidth={2.5} />
+          </span>
+          Je quitte
+        </span>
+        <ArrowRight className="h-[0.8em] w-[0.8em]" strokeWidth={2.5} />
+      </Link>
       </div> 
     </section>
   )
--- src/App/5000--VisitorsSection/100--VisitorsMainPage/VisitorsMainPage.tsx
+++ src/App/5000--VisitorsSection/100--VisitorsMainPage/VisitorsMainPage.tsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom"
 import { useLocation } from "react-router-dom"
 import SuccesBox from "../Components/SuccesBox";
 import { useVisitors } from "../Contexts/VisitorsContext/UseVisitors";
+import vegibecFlower from '../assets/images/vegibec_flower.png'
 
 
 
@@ -18,8 +19,18 @@ if (sessionSubmissionSuccess) {
 
 
   return (
-    <article className={`relative flex flex-col items-center font-secondary pb-4 max-sm[0.85rem] lg:mt-0 tablet:justify-center tablet:min-h-screen  ${location.pathname === '/visiteurs' ? 'tablet:-mt-5' : ''}`}>
-      <h2 className="text-[3.5em] font-bold font-primary text-secondary text-center">Registre des visiteurs</h2>
+    <article className={`relative flex flex-col items-center font-secondary pb-4 max-sm[0.85rem] lg:mt-0 tablet:justify-center tablet:min-h-screen  ${location.pathname === '/visiteurs' ? 'tablet:-mt-10' : ''}`}>
+        <div className="relative ">
+          <div className="absolute top-1/2 -translate-y-1/2 right-40 w-[120%] h-0.5 bg-linear-to-r from-secondary/20 to-secondary"></div>
+        <img src={vegibecFlower} alt="Vegibec Flower" className="w-35"  />
+        <div className="absolute top-1/2 -translate-y-1/2 left-40 w-[120%] h-0.5 bg-linear-to-rrom-secondary to-secondary/20"></div>
+</div>
+<div className="relative mb-10">
+      <h2 className="text-[3.5em] font-bold font-primary text-secondary text-center leading-tight">Registre des visiteurs</h2>
+      <div className="bg-linear-to-r from-secondary/20 to-secondary rounded-full h-0.5 w-[25%] absolute right-[42%] top-[145%] -translate-y-1/2 -translate-x-1/2"></div>
+      <div className="bg-secondary rounded-full h-2.5 w-2.5 absolute left-1/2 top-[145%] -translate-y-1/2 -translate-x-1/2"></div>
+      <div className="bg-linear-to-r from-secondary to-secondary/20 rounded-full h-0.5 w-[25%] absolute left-[67%] top-[145%] -translate-y-1/2 -translate-x-1/2"></div>
+      </div>
        <Outlet />
     </article>
   )
