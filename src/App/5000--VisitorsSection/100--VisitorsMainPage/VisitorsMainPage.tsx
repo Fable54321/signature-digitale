af4cdbdbddb174c83ac0eb493d0b33bada7884e7
fable54321 <timothe5@hotmail.com>
2026-05-04 09:56:42 -0400

    first commit

--- src/App/5000--VisitorsSection/100--VisitorsMainPage/VisitorsMainPage.tsx
+++ src/App/5000--VisitorsSection/100--VisitorsMainPage/VisitorsMainPage.tsx
@@ -1,10 +1,16 @@
 import { Outlet } from "react-router-dom"
+import { useLocation } from "react-router-dom"
 
 
 
 const VisitorsMainPage = ( { token } : { token: string } ) => {
+
+
+const location = useLocation();
+
+
   return (
-    <article className=" flex flex-col items-center font-secondary pb-4 max-sm[0.85rem] lg:mt-0 tablet:justify-center tablet:min-h-screen tablet:-mt-5">
+    <article className={`flex flex-col items-center font-secondary pb-4 max-sm[0.85rem] lg:mt-0 tablet:justify-center tablet:min-h-screen  ${location.pathname === '/visiteurs' ? 'tablet:-mt-5' : ''}`}>
       <h2 className="text-[3.5em] font-bold font-primary text-secondary text-center">Registre des visiteurs</h2>
       <Outlet context={{ token }} />
     </article>
