9f23330f74d8d9e4bbd7c6247a7e7b1886690849
fable54321 <timothe5@hotmail.com>
2026-05-04 11:35:53 -0400

    first commit

--- src/App/5000--VisitorsSection/100--VisitorsMainPage/VisitorsMainPage.tsx
+++ src/App/5000--VisitorsSection/100--VisitorsMainPage/VisitorsMainPage.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom"
 import { useLocation } from "react-router-dom"
+import SuccesBox from "../Components/SuccesBox";
 
 
 
@@ -10,8 +11,9 @@ const location = useLocation();
 
 
   return (
-    <article className={`flex flex-col items-center font-secondary pb-4 max-sm[0.85rem] lg:mt-0 tablet:justify-center tablet:min-h-screen  ${location.pathname === '/visiteurs' ? 'tablet:-mt-5' : ''}`}>
+    <article className={`relative flex flex-col items-center font-secondary pb-4 max-sm[0.85rem] lg:mt-0 tablet:justify-center tablet:min-h-screen  ${location.pathname === '/visiteurs' ? 'tablet:-mt-5' : ''}`}>
       <h2 className="text-[3.5em] font-bold font-primary text-secondary text-center">Registre des visiteurs</h2>
+        <SuccesBox />
       <Outlet context={{ token }} />
     </article>
   )
---
+++ src/App/5000--VisitorsSection/Components/SuccesBox.tsx
@@ -0,0 +1,13 @@
+import success from "../assets/images/check.png"
+
+
+const SuccesBox = () => {
+  return (
+    <div className="test px-8 w-[min(98%,500px)] absolute z-51 top-[35%] left-1/2 -translate-x-1/2 border-4 flex flex-col items-center gap-8 py-4 border-secondary border-t-0 border-r-0 border-b-8 bg-white rounded-3xl">
+      <p className="bg-tertiary rounded-xl  text-center font-primary font-bold text-secondary text-[2.5em] ">Les informations du visiteurs ont été enregistrées avec succès</p>
+      <img className="bg-tertiary p-1 rounded-xl" src={success} alt="Success" />
+    </div>
+  )
+}
+
+export default SuccesBox
